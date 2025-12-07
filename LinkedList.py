344f4ab1a2add72e2d4d3004475837596c4e9fb1
Leonid Veselovskiy <leonid.veselovskii@gmail.com>
2025-12-07 14:42:22 +0300

    Enhance LinkedList

--- LinkedList.py
+++ LinkedList.py
@@ -15,31 +15,97 @@ class LinkedList:
         '''
         Link to the first element
         '''
-        self.head = None
+        self.top = Node()
+        self.tail = None
+        self.len = 0
 
     def append(self, value):
         '''
         Append a value to LinkedList
+        O(1)
+        '''
+        new_node = Node(value)
+        if self.tail:
+            self.tail.next_node = new_node
+        else:
+            self.top.next_node = new_node
+        self.tail = new_node
+        self.len += 1
+
+    def prepend(self, value):
+        '''
+        Insert value to the beginning of LinkedList
+        O(1)
+        '''
+        new_node = Node(value)
+        new_node.next_node = self.top.next_node
+        self.top.next_node = new_node
+        if self.tail is None:
+            self.tail = new_node
+        self.len += 1
+
+    def find(self, target):
+        '''
+        Find the first node with value
         O(N)
         '''
-        # Set value as head if LinkedList is empty
-        if self.head is None:
-            self.head = Node(value)
-            return
-        
-        # Find Node without next_node
-        current = self.head
-        while current.next_node is not None:
+        current = self.top.next_node
+        while current:
+            if current.value == target:
+                return current
             current = current.next_node
-        
-        # Set value as next_node for current Node
-        current.next_node = Node(value)
+        return None
     
+    def insert(self, value, target):
+        '''
+        Insert value after target
+        O(N)
+        '''
+        target_node = self.find(target)
+        if target_node is None:
+            return -1
+        
+        new_node = Node(value)
+        new_node.next_node = target_node.next_node
+        target_node.next_node = new_node
+        if self.tail == target_node:
+            self.tail = new_node   
+        self.len += 1
+        return
+
+    def delete(self, target):
+        '''
+        Delete the first node with value
+        O(N)
+        '''
+        current_node = self.top.next_node
+        prev_node = self.top
+
+        while current_node is not None:
+            if current_node.value == target:
+                prev_node.next_node = current_node.next_node
+                self.len -= 1
+                if current_node == self.tail:
+                    if prev_node == self.top:
+                        self.tail = None
+                    else:
+                        self.tail = prev_node
+                return
+            prev_node = current_node
+            current_node = current_node.next_node
+        return -1
+
+    def __len__(self):
+        '''
+        Return length of LinkedList
+        '''
+        return self.len
+   
     def __str__(self):
         '''
         Return all elements as a string
         '''
-        current = self.head
+        current = self.top.next_node
         values = "["
 
         while current is not None:
